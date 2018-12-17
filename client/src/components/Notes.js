9dd9eb2b7a7959d887bef5835918d2237c0547f5
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-17 15:37:36 -0700

    get notes

--- client/src/components/Notes.js
+++ client/src/components/Notes.js
@@ -2,16 +2,31 @@ import React from 'react';
 import { connect } from 'react-redux' 
 import { Link } from 'react-router-dom'
 import { getNotes } from '../reducers/notes'
-import {Card} from 'semantic-ui-react'
+import { Card, Container } from 'semantic-ui-react'
 
 class Notes extends React.Component {
   componentDidMount() {
     this.props.dispatch(getNotes())
   }
+
+  renderNotes = () => {
+    return this.props.notes.map( note => 
+      <Card key={note.id}>
+        <Card.Content>
+          <Card.Header>{note.title}</Card.Header>
+          <Card.Description>{note.description}</Card.Description>
+        </Card.Content>
+      </Card>
+    )
+  }
   
   render() {
     return (
-      null
+      <Container>
+        <Card.Group>
+          {this.renderNotes()}
+        </Card.Group>
+      </Container>
     )
   }
 }
