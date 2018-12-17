b811e2d24aa2c6d687da204bd5ab172e47da0b7a
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-17 13:23:51 -0700

    initial client side

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import store from './store'
-import './index.css';
 import App from './App';
 
 
