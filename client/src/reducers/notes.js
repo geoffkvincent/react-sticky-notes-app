49cb1b310c0f60275e6a175c3af8988c032eafde
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-17 14:28:13 -0700

    redux reducers

--- client/src/reducers/notes.js
+++ client/src/reducers/notes.js
@@ -33,3 +33,13 @@ export const deleteNote = (id) => {
   }
 }
 
+export default ( state = [], action ) => {
+  switch(action.type) {
+    case NOTES:
+      return action.notes
+    case ADD_NOTE:
+      return [ action.note, ...state ]
+    case UPDATE_NOTE:
+      return state.map( )
+  }
+}
