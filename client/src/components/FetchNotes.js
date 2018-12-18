fa3b5329704ce5de48d082974f43a5e6b74c2959
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-18 16:21:55 -0700

    fetch note

--- client/src/components/FetchNotes.js
+++ client/src/components/FetchNotes.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Route} from 'react-router-dom'
-import Notes from './components/Notes'
-import Note from './components/Note'
-import {getNotes} from './reducers/note'
+import Notes from '.Notes'
+import Note from './Note'
+import {getNotes} from '../reducers/note'
 import {Loader, Segment, Dimmer } from './semantic-ui-react'
 
 class FetchNotes extends React.Component {
