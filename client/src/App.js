54541a74bda188aaba7416b4866c0383d189cbe6
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-18 16:09:48 -0700

    fetch note

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import Notes from './components/Notes'
 import Note from './components/Note'
 import NoteForm from './components/NoteForm'
 import Navbar from './components/Navbar'
+import FetchNotes from './components/FetchNotes'
 
 const App = () => (
   <Fragment>
     <Navbar />
     <Switch>
+      
       <Route exact path="/" component={Notes} />
       <Route exact path ="/notes/new" component={NoteForm} />
       <Route exact path="/note/:id" component={Note} />
---
+++ client/src/components/FetchNotes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Route} from 'react-router-dom'
+import Notes from './components/Notes'
+import Note from './components/Note'
+import {getNotes} from './reducers/note'
+import {Loader, Segement, } from './semantic-ui-react'
+
+class Fetch
