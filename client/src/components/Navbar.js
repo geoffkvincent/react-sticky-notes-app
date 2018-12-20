a34914d91f079f96fdb5a726cf045f0d6665fe25
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-20 11:25:09 -0700

    style

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
+import styled from 'styled-components'
 
 const Navbar = () => (
-  <Menu>
-    <Menu.Item>
-      <NavLink exact to="/">
-        Home
-      </NavLink>
-    </Menu.Item>
-  </Menu>
+  <NavContainer>
+    <Menu>
+      <Menu.Item>
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+      </Menu.Item>
+    </Menu>
+  </NavContainer>
 )
-  
+
+const NavContainer = styled.div `
+  background: linear-gradient(to bottom right, black, rgb(0, 255, 98));
+  color: red
+`
+
 export default Navbar
