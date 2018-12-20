b1105960ffd8d3dc2c87725c4d4cda4fac19d386
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-20 10:25:39 -0700

    style

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,18 @@ import { Route, Switch } from 'react-router-dom';
 import NoteForm from './components/NoteForm'
 import Navbar from './components/Navbar'
 import FetchNotes from './components/FetchNotes'
+import {Container} from 'semantic-ui-react'
 
 
 const App = () => (
-  <Fragment>
+  <Container>
     <Navbar />
     <Switch>
       <Route path='/' component={FetchNotes} />
       
       <Route exact path="/note/:id/edit" component={NoteForm} />
     </Switch>
-  </Fragment>  
+  </Container>  
 )
 
 export default App;
